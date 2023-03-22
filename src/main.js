14549eba0e6527c3b275a5bcc12cdcba2f8cb6df
Helio Morais <heliogov20@gmail.com>
2023-03-22 19:03:49 -0300

    Requisito 3. VAMOS COM TUDO :rocket:

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const renderProducts = async () => {
+  try {
+    const products = await fetchProductsList('computador');
+    const productsSection = document.querySelector('.products');
+
+    products.forEach((product) => {
+      const productElement = createProductElement(product);
+      productsSection.appendChild(productElement);
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+renderProducts();
