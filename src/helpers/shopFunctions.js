4542e5f83144e9519fbeb58b21e2084aadf8f0a7
Helio Morais <heliogov20@gmail.com>
2023-03-23 20:28:56 -0300

    Requisito 8  :rocket:

--- src/helpers/shopFunctions.js
+++ src/helpers/shopFunctions.js
@@ -1,4 +1,5 @@
-import { removeCartID } from './cartFunctions';
+import { removeCartID, saveCartID } from './cartFunctions';
+import { fetchProduct } from './fetchFunctions';
 
 // Esses comentários que estão antes de cada uma das funções são chamados de JSdoc,
 // experimente passar o mouse sobre o nome das funções e verá que elas possuem descrições!
@@ -122,6 +123,14 @@ export const createProductElement = ({ id, title, thumbnail, price }) => {
     'Adicionar ao carrinho!',
   );
   section.appendChild(cartButton);
+
+  cartButton.addEventListener('click', async () => {
+    saveCartID(id);
+    const products = document.querySelector('.cart__products');
+    const paramID = await fetchProduct(id);
+    const productsSection = createCartProductElement(paramID);
+    products.appendChild(productsSection);
+  });
 
   return section;
 };
